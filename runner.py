778afa761a4d0d46061a8aeaee5fe58d21b84b4d
wgwz <klawlor419@gmail.com>
2017-01-21 21:12:39 -0500

    3

--- runner.py
+++ runner.py
@@ -1,8 +1,9 @@
+from pprint import pprint
 from my_package.myfirstmodule.maps import maps
-from my_ackage.mysecondmodule.utils import connection_to
+from my_package.mysecondmodule.utils import connection_to
 
 def viz():
     return maps, connection_to
 
 if __name__ == '__main__':
-    print viz()
+    pprint(viz())
