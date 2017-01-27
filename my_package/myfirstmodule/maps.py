738fb0c26b2b25121643ad180df0c53f9864c684
wgwz <klawlor419@gmail.com>
2017-01-27 11:36:38 -0500

    mocks

--- my_package/myfirstmodule/maps.py
+++ my_package/myfirstmodule/maps.py
@@ -1,3 +1,19 @@
+import requests
 from my_package.mysecondmodule.utils import connection_to
 
+
 maps = connection_to + 'maapps'
+
+class Send(object):
+
+    def __init__(self):
+        self.resp = None
+
+    def ping(self):
+
+        self.resp = requests.get("http://google.com")
+
+        if self.resp.status_code == 200:
+            return "success"
+
+        return "failure"
--- test.py
+++ test.py
@@ -1,5 +1,56 @@
-from my_package.myfirstmodule.maps import maps
+import pytest
+from mock import patch
+
+from my_package.myfirstmodule.maps import maps, Send
 from my_package.mysecondmodule.utils import connection_to
+
+
+# Basic test
 
 def test_maps_object():
     assert connection_to in maps
+
+
+# Mocking
+
+@patch('my_package.myfirstmodule.maps.requests.get')
+def test_get_request_called_once(mock_get):
+    send = Send()
+    send.ping()
+    mock_get.assert_called_once_with('http://google.com')
+
+@patch('my_package.myfirstmodule.maps.requests.get')
+def test_get_request_not_200(mock_get):
+    send = Send()
+    mock_get.return_value.status_code = 'not 200'
+    assert send.ping() == 'failure'
+
+@patch('my_package.myfirstmodule.maps.requests.get')
+def test_get_request_200(mock_get):
+    send = Send()
+    mock_get.return_value.status_code = 200
+    assert send.ping() == 'success'
+
+
+# Same as the last three tests but with some duplication removed
+# Thanks for the fancy footwork, pytest and mock libraries :)
+
+@pytest.fixture(scope='session')
+def send(): 
+    
+    yield Send()
+
+@patch('my_package.myfirstmodule.maps.requests.get')
+class TestSend(object):
+
+    def test_get_request_called_once(self, mock_get, send):
+        send.ping()
+        mock_get.assert_called_once_with('http://google.com')
+
+    def test_get_request_not_200(self, mock_get, send):
+        mock_get.return_value.status_code = 'not 200'
+        assert send.ping() == 'failure'
+
+    def test_get_request_200(self, mock_get, send):
+        mock_get.return_value.status_code = 200
+        assert send.ping() == 'success'
